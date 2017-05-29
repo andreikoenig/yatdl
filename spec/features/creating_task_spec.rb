b0fbf4dee1eb8a0c75c28e6e2e943fbfd3080057
Andrei Koenig <nv1982@gmail.com>
2017-05-29 19:25:55 -0400

    can directly add tasks to each quadrant, update feature test

--- spec/features/creating_task_spec.rb
+++ spec/features/creating_task_spec.rb
@@ -1,12 +1,51 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Tasks" do
-  scenario "a user creates a new task" do
+  scenario "a user creates a new task in quadrant 1" do
     visit "/"
-    fill_in "task_description", with: "Sample Task"
-    select "2", from: "task_quadrant"
-    click_button "Create Task"
-    expect(page).to have_content("Sample Task")
+
+    within("#quad1") do
+      fill_in "task_description", with: "Sample Task 1"
+      click_button "Create Task"
+    end
+
+    expect(find("#quad1")).to have_content("Sample Task 1")
+    expect(page.current_path).to eq(root_path)
+  end
+
+  scenario "a user creates a new task in quadrant 2" do
+    visit "/"
+
+    within("#quad2") do
+      fill_in "task_description", with: "Sample Task 2"
+      click_button "Create Task"
+    end
+
+    expect(find("#quad2")).to have_content("Sample Task 2")
+    expect(page.current_path).to eq(root_path)
+  end
+
+  scenario "a user creates a new task in quadrant 3" do
+    visit "/"
+
+    within("#quad3") do
+      fill_in "task_description", with: "Sample Task 3"
+      click_button "Create Task"
+    end
+
+    expect(find("#quad3")).to have_content("Sample Task 3")
+    expect(page.current_path).to eq(root_path)
+  end
+
+  scenario "a user creates a new task in quadrant 4" do
+    visit "/"
+
+    within("#quad4") do
+      fill_in "task_description", with: "Sample Task 4"
+      click_button "Create Task"
+    end
+
+    expect(find("#quad4")).to have_content("Sample Task 4")
     expect(page.current_path).to eq(root_path)
   end
 end
